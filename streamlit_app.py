46a2141bc268dd6942e3293c0cf278ba1302e93c
zouaoui-j <124874649+zouaoui-j@users.noreply.github.com>
2024-10-29 16:19:39 +0100

    Update streamlit_app.py
    format fruityvice response to json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
